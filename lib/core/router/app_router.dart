e70acd0d4a613ebf94ed473377fa04b652dde3ae
simanoyuki <yuukireirei5@gmail.com>
2026-01-02 16:21:07 +0900

    feat: フォロー中のユーザー一覧画面を追加
    - FollowingUsersScreenを作成
    - フォロー中のユーザー一覧を表示
    - 各ユーザーをタップでOtherUserContributionScreenに遷移
    - ルーターに/followingルートを追加
    
    Closes #73, #74, #75, #76, #77, #78, #79, #80, #81, #82

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -2,15 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../../shared/widgets/main_navigation.dart';
 import '../../features/github_contribution/presentation/screens/contribution_statistics_screen.dart';
+import '../../features/github_contribution/presentation/screens/other_user_contribution_screen.dart';
+import '../../features/github_contribution/presentation/screens/following_users_screen.dart';
 import '../../features/github_contribution/domain/entities/contribution_statistics.dart';
 
 final GoRouter appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const MainNavigation(),
-    ),
+    GoRoute(path: '/', builder: (context, state) => const MainNavigation()),
     GoRoute(
       path: '/statistics',
       pageBuilder: (context, state) {
@@ -25,6 +24,20 @@ final GoRouter appRouter = GoRouter(
           ),
         );
       },
+    ),
+    GoRoute(
+      path: '/user/:username',
+      pageBuilder: (context, state) {
+        final username = state.pathParameters['username']!;
+        return MaterialPage(
+          key: state.pageKey,
+          child: OtherUserContributionScreen(username: username),
+        );
+      },
+    ),
+    GoRoute(
+      path: '/following',
+      builder: (context, state) => const FollowingUsersScreen(),
     ),
     GoRoute(
       path: '/settings',
